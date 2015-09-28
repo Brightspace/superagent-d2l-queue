e286b8bfa81acf856eaf710a8e8398ff600dd37c
Mike Dedys <Michal.Dedys@D2L.com>
2015-09-28 11:32:47 -0400

    - Travis now passes

--- src/RequestQueue.js
+++ src/RequestQueue.js
@@ -16,7 +16,7 @@ import Auth from 'superagent-d2l-session-auth';
 const RETRY_INTERVAL = 2000;
 const REQUEST_TIMEOUT = 15000;
 
-class RequestUtils extends {
+class RequestUtils {
 
 	_bind( ...handlers ) {
 		handlers.forEach( handler => this[handler] = this[handler].bind( this ) );
